e48cc97f6420eb2a17ab64cfe1d47f150295ff6a
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2022-10-03 16:05:15 +1100

    Implement playRound function

--- script.js
+++ script.js
@@ -14,4 +14,24 @@ function getComputerChoice() {
   return choice;
 }
 
-getComputerChoice()
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  console.log(playerSelection)
+  console.log(computerSelection)
+  let outcome = "";
+  if (playerSelection == computerSelection) {
+    outcome = "tie";
+  } else if ((playerSelection == "rock" && computerSelection == "scissors") ||
+  (playerSelection == "paper" && computerSelection == "rock") ||
+  (playerSelection == "scissors" && computerSelection == "paper")) {
+    outcome = "player";
+  } else {
+    outcome = "computer";
+  }
+
+  return outcome;
+}
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+console.log (playRound(playerSelection, computerSelection));
