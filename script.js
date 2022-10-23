aec294821c98af13f7d21d37a188254faae70f2b
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2022-10-23 11:17:41 +1100

    Add event listener for each button

--- script.js
+++ script.js
@@ -69,4 +69,10 @@ function game() {
   }
 }
 
-game()
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    alert(button.id);
+  });
+});
