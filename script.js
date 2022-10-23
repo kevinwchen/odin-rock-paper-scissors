0ab2c505e1afcb4c955aae77b1f29d6755d7ebcf
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2022-10-23 20:39:15 +1100

    Implement selection, score, round winner, and restart game functions

--- script.js
+++ script.js
@@ -10,69 +10,70 @@ function getComputerChoice() {
   } else if (generatedChoice == 2) {
     choice = "scissors";
   }
-
   return choice;
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection, selection, announce) {
   playerSelection = playerSelection.toLowerCase();
+  selection.textContent = "You chose " + playerSelection + "."
   if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors" ) {
     return undefined;
   }
   let outcome = "";
   if (playerSelection == computerSelection) {
     outcome = "tie";
-    console.log("Tie, no winner this round.")
-    alert("Tie, no winner this round.")
+    announce.textContent = "Tie, no winner this round."
   } else if ((playerSelection == "rock" && computerSelection == "scissors") ||
     (playerSelection == "paper" && computerSelection == "rock") ||
     (playerSelection == "scissors" && computerSelection == "paper")) {
     outcome = "player";
-    console.log("Player wins the round!")
-    alert("Player wins the round!")
+    announce.textContent = "Player wins the round!"
+    playerScore += 1;
   } else {
     outcome = "computer";
-    console.log("Computer wins the round!")
-    alert("Computer wins the round!")
+    announce.textContent = "Computer wins the round!"
+    computerScore += 1;
   }
 
   return outcome;
 }
 
-function game() {
-  let roundsTotal = 5;
-  let playerScore = computerScore = 0;
-
-  for (let i = 0; i < roundsTotal; i++) {
-    let computerSelection = getComputerChoice();
-    let playerSelection = prompt("Enter rock, paper, or scissors:");
-    let outcome = playRound(playerSelection, computerSelection);
-    
-    // Track the score based on the outcome returned
-    if (outcome == "player") {
-      playerScore += 1;
-    } else if (outcome == "computer") {
-      computerScore += 1;
-    }
-  }
-
-  // Print the results of the game
-  if (playerScore > computerScore) {
-    console.log("Player wins the game, congratulations!");
-    alert("Player wins the game, congratulations!");
-  } else if (playerScore < computerScore) {
-    console.log("Computer wins the game, better luck next time.");
-    alert("Computer wins the game, better luck next time.");
-  } else {
-    console.log("Tie game.");
-    alert("Tie game.");
-  }
+function updateScore() {
+  playerScoreText.textContent = `Player Score: ${playerScore}`;
+  computerScoreText.textContent = `Computer Score: ${computerScore}`;
 }
 
-const buttons = document.querySelectorAll('button');
+// function checkWinner() {
+//   if (playerScore === 5) {
+//     gameButtons.disabled = true;
+//   }
+// }
 
-buttons.forEach((button) => {
+function restartGame() {
+  playerScore = computerScore = 0;
+  updateScore();
+  selection.textContent = announce.textContent = "";
+}
+
+const gameButtons = document.querySelectorAll('.gameBtn');
+const selection = document.querySelector('.selection');
+const announce = document.querySelector('.announce');
+const restartButton = document.querySelector('#restart');
+const playerScoreText = document.querySelector('.player-score');
+const computerScoreText = document.querySelector('.computer-score');
+const winner = document.querySelector('.winner');
+
+let playerScore = 0;
+let computerScore = 0;
+
+gameButtons.forEach((button) => {
   button.addEventListener('click', () => {
-    alert(button.id);
+    playRound(button.id, getComputerChoice(), selection, announce);
+    updateScore();
+    // checkWinner();
   });
+});
+
+restartButton.addEventListener('click', () => {
+  restartGame();
 });
